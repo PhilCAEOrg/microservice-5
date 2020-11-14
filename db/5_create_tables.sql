e7eb06520c667c1cf71070130326d55e33401610
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-11-14 18:02:33 +0000

    Init

---
+++ db/5_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeshc
+-- Automatically generated sql script for the service 5, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table sdf.
+--
+CREATE TABLE caeshc.sdf (
+  sds Integer ,
+CONSTRAINT sds_PK PRIMARY KEY (sds)
+ 
+);
+
+
+
